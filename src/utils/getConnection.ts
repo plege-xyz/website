50c923f21b66f0d542dfe4e88fa932ed2171cb38
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2022-11-01 17:29:17 +0530

    update rpc

--- src/utils/getConnection.ts
+++ src/utils/getConnection.ts
@@ -4,9 +4,7 @@ export const getConnection = (network: "MAINNET" | "TESTNET" | "DEVNET") => {
   if (network === "TESTNET") {
     return new Connection(clusterApiUrl("testnet"), "confirmed");
   } else if (network === "DEVNET") {
-    return new Connection(
-      "https://hidden-sleek-haze.solana-devnet.discover.quiknode.pro/3e85aefa088db554d67f6e632694dcc10f320b58/"
-    );
+    return new Connection(clusterApiUrl("devnet"), "confirmed");
   }
 
   return new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
