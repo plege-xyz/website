679ff221f0c3e1b818e2bd9f48ebd203b85682e0
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2022-11-16 02:33:54 +0530

    Update login.tsx

--- src/pages/dashboard/login.tsx
+++ src/pages/dashboard/login.tsx
@@ -34,7 +34,7 @@ const Dashboard = () => {
 
   return (
     <Layout>
-      <div className="flex h-full w-full items-center justify-center">
+      <div className="flex h-[calc(100vh-4rem)] w-full items-center justify-center">
         <Wallet />
       </div>
     </Layout>
