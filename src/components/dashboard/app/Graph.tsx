b78918c5469d0f0418c478b0ed665094f104138b
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-01-06 07:49:56 +0530

    wip analytics

--- src/components/dashboard/app/Graph.tsx
+++ src/components/dashboard/app/Graph.tsx
@@ -1,78 +1,124 @@
-import { overpass, tt, _space } from "@/utils/fonts";
+import { overpass, _overpass } from "@/utils/fonts";
 import { Bar } from "react-chartjs-2";
+import type { ChartData, ChartOptions } from "chart.js";
 import { Chart as ChartJS, registerables } from "chart.js";
+import type { Subscription, Tier } from "plege";
 ChartJS.register(...registerables);
 
-const Graph = () => {
-  const labels = Array.from({ length: 30 }, (_, i) => {
-    const date = new Date();
-    date.setDate(date.getDate() - i);
-    return `${date.toLocaleString("default", {
-      month: "short",
-    })} ${date.getDate()}`;
-  }).reverse();
+const Graph = ({
+  subscriptions,
+  tiers,
+}: {
+  subscriptions: Subscription[];
+  tiers: Tier[];
+}) => {
+  function getLast30Days() {
+    const today = new Date();
+    const dates = [];
 
-  const data = labels.map(() => {
-    // random number between 0 and 50
-    return Math.floor(Math.random() * 50);
-  });
+    for (let i = 0; i < 30; i++) {
+      const date = new Date(today);
+      date.setDate(date.getDate() - i);
+      dates.push(date);
+    }
 
-  const options = {};
+    return dates.reverse();
+  }
+
+  const dates = getLast30Days();
+  const formattedDates = dates.map((date) =>
+    date.toLocaleDateString("en-US", { month: "short", day: "numeric" })
+  );
+
+  const data: ChartData<"bar"> = {
+    labels: formattedDates,
+    datasets: tiers
+      .sort((a, b) => {
+        return a.price - b.price;
+      })
+      .map((tier, i) => {
+        const tierSubscriptions = subscriptions.filter(
+          (subscription) => subscription.tier === tier.publicKey
+        );
+
+        return {
+          label: tier.name,
+          data: dates.map((date) => {
+            const subscriptionsOnDate = tierSubscriptions.filter(
+              (subscription) => {
+                const startDate = subscription.start;
+                const payPeriodStartDate = subscription.payPeriodStart;
+                return (
+                  (startDate.getDay() === date.getDay() &&
+                    startDate.getMonth() === date.getMonth() &&
+                    startDate.getFullYear() === date.getFullYear()) ||
+                  (payPeriodStartDate.getDay() === date.getDay() &&
+                    payPeriodStartDate.getMonth() === date.getMonth() &&
+                    payPeriodStartDate.getFullYear() === date.getFullYear())
+                );
+              }
+            );
+            return subscriptionsOnDate.length;
+          }),
+          borderColor: "#000000",
+          backgroundColor: "#fff",
+          borderSkipped: false,
+        };
+      }),
+  };
+
+  const options: ChartOptions<"bar"> = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        titleFont: {
+          family: _overpass.style.fontFamily,
+          size: 18,
+        },
+        bodyFont: {
+          family: _overpass.style.fontFamily,
+          lineHeight: 0,
+        },
+        titleMarginBottom: 10,
+        borderColor: "#fff",
+        borderWidth: 1,
+        padding: 15,
+        boxPadding: 5,
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          autoSkip: true,
+          maxTicksLimit: 8,
+          maxRotation: 0,
+          font: {
+            family: _overpass.style.fontFamily,
+          },
+        },
+        stacked: true,
+      },
+      y: {
+        ticks: {
+          autoSkip: true,
+          maxTicksLimit: 5,
+          maxRotation: 0,
+          font: {
+            family: _overpass.style.fontFamily,
+          },
+        },
+      },
+    },
+  };
 
   return (
-    <div className="relative h-full w-full cursor-pointer rounded p-5 py-8">
-      <div className="absolute flex h-full w-full items-center justify-center">
-        <div className={`${tt} z-50 -mt-10 text-xl uppercase text-gray-300`}>
-          coming soon
-        </div>
-      </div>
-      <div className="w-full h-full blur-sm">
-        <Bar
-          options={{
-            responsive: true,
-            plugins: {
-              legend: {
-                display: false,
-              },
-            },
-            scales: {
-              x: {
-                ticks: {
-                  autoSkip: true,
-                  maxTicksLimit: 5,
-                  maxRotation: 0,
-                  font: {
-                    family: _space.style.fontFamily,
-                  },
-                  color: "#c5c5c5",
-                },
-              },
-              y: {
-                ticks: {
-                  autoSkip: true,
-                  maxTicksLimit: 5,
-                  maxRotation: 0,
-                  font: {
-                    family: _space.style.fontFamily,
-                  },
-                  color: "#c5c5c5",
-                },
-              },
-            },
-          }}
-          data={{
-            labels: labels,
-            datasets: [
-              {
-                label: "Subscriptions",
-                data,
-                backgroundColor: "#222",
-                borderColor: "#222",
-                borderWidth: 1,
-              },
-            ],
-          }}
-        />
+    <div className="relative h-full w-full cursor-pointer rounded pl-5 pt-5 pr-5 pb-3">
+      <div className="h-full w-full">
+        <Bar options={options} data={data} />
       </div>
     </div>
   );
--- src/pages/dashboard/[app]/analytics.tsx
+++ src/pages/dashboard/[app]/analytics.tsx
@@ -1,32 +1,170 @@
 import Layout from "@/components/dashboard/Layout";
 import { overpass, tt } from "@/utils/fonts";
+import { Plege } from "@/utils/plege";
 import { useRouter } from "next/router";
+import { FakeWallet } from "plege";
+import type { Subscription, Tier } from "plege";
+import { useEffect, useState } from "react";
+import Graph from "@/components/dashboard/app/Graph";
+import Loader from "@/components/Loader";
 
 const App = () => {
+  const [subscriptions, setSubscriptions] = useState<Subscription[]>();
+  const [tiers, setTiers] = useState<Tier[]>();
+
   const router = useRouter();
   const app = router.query.app as string;
 
+  useEffect(() => {
+    if (app && !tiers) {
+      const getSubscriptions = async () => {
+        return setSubscriptions(
+          await Plege(FakeWallet).subscription.all({
+            app,
+          })
+        );
+      };
+
+      const getTiers = async () => {
+        return setTiers(
+          await Plege(FakeWallet).tier.all({
+            app,
+          })
+        );
+      };
+
+      getSubscriptions();
+      getTiers();
+    }
+  }, [app, tiers]);
+
+  const getMRR = () => {
+    if (!subscriptions || !tiers) return;
+
+    // Get the current date
+    const currentDate = new Date();
+
+    let mrr = 0;
+    subscriptions.forEach((subscription) => {
+      // Check if the subscription is still active
+      if (subscription.payPeriodExpiration > currentDate) {
+        // Find the tier for the subscription
+        const tier = tiers.find((t) => t.publicKey === subscription.tier);
+        if (tier) {
+          // Calculate the monthly amount for the subscription
+          let monthlyAmount = tier.price;
+          if (tier.interval === "YEARLY") {
+            monthlyAmount /= 12;
+          }
+          // Add the monthly amount to the MRR
+          mrr += monthlyAmount;
+        }
+      }
+    });
+    // Return the MRR as a string in the desired format
+    return "$" + mrr.toLocaleString();
+  };
+
+  const getARR = () => {
+    if (!subscriptions || !tiers) return;
+
+    // Get the current date
+    const currentDate = new Date();
+
+    let arr = 0;
+    subscriptions.forEach((subscription) => {
+      // Check if the subscription is still active
+      if (subscription.payPeriodExpiration > currentDate) {
+        // Find the tier for the subscription
+        const tier = tiers.find((t) => t.publicKey === subscription.tier);
+        if (tier) {
+          // Calculate the annual amount for the subscription
+          let annualAmount = tier.price;
+          if (tier.interval === "MONTHLY") {
+            annualAmount *= 12;
+          }
+          // Add the annual amount to the ARR
+          arr += annualAmount;
+        }
+      }
+    });
+    // Return the ARR as a string in the desired format
+    return "$" + arr.toLocaleString();
+  };
+
+  const getSubscribers = () => {
+    if (!subscriptions) return;
+
+    return subscriptions
+      .filter((subscription) => {
+        return subscription.payPeriodExpiration > new Date();
+      })
+      .length.toString();
+  };
+
+  const getChurn = () => {
+    if (!subscriptions) return;
+
+    // Get the current date
+    const currentDate = new Date();
+
+    let expiredCount = 0;
+    let startCount = 0;
+    subscriptions.forEach((subscription) => {
+      // Check if the subscription expired in the previous month
+      let prevMonth = currentDate.getMonth() - 1;
+      let prevYear = currentDate.getFullYear();
+      if (prevMonth < 0) {
+        prevMonth += 12;
+        prevYear--;
+      }
+      if (
+        subscription.payPeriodExpiration.getMonth() === prevMonth &&
+        subscription.payPeriodExpiration.getFullYear() === prevYear
+      ) {
+        expiredCount++;
+      }
+      // Check if the subscription was active at the start of the previous month
+      if (
+        subscription.payPeriodStart < currentDate &&
+        subscription.payPeriodExpiration > currentDate
+      ) {
+        startCount++;
+      }
+    });
+    // Return 0% if the number of expired subscriptions is 0 or the number of subscriptions at the start of the previous month is 0
+    if (expiredCount === 0 || startCount === 0) {
+      return "0%";
+    }
+    // Calculate the churn rate as a percentage and round to two decimal places
+    const churnRate = ((expiredCount / startCount) * 100).toFixed(2);
+
+    // Return the churn rate as a string with a percentage sign
+    return churnRate + "%";
+  };
+
   const stats: {
     label: string;
-    value: string;
+    value?: string;
   }[] = [
     {
       label: "MRR",
-      value: "$1,234",
+      value: getMRR(),
     },
     {
       label: "ARR",
-      value: "$12,234",
+      value: getARR(),
     },
     {
       label: "SUBSCRIBERS",
-      value: "425",
+      value: getSubscribers(),
     },
     {
       label: "CHURN",
-      value: "4.2%",
+      value: getChurn(),
     },
   ];
+  console.log(subscriptions);
 
   return (
     <Layout>
@@ -42,14 +180,24 @@ const App = () => {
                   <div className={`${overpass} text-lg text-gray-400`}>
                     {stat.label}
                   </div>
-                  <div className={`${tt} text-4xl`}>{stat.value}</div>
+                  {stat.value ? (
+                    <div className={`${tt} text-4xl`}>{stat.value}</div>
+                  ) : (
+                    <div className="h-10 w-36 animate-pulse rounded bg-[#111]"></div>
+                  )}
                 </div>
               ))}
             </div>
             <div
-              className={`flex h-96 items-center justify-center text-lg ${overpass}`}
+              className={`flex h-[30rem] items-center justify-center text-lg ${overpass}`}
             >
-              soonTM
+              {subscriptions && tiers ? (
+                <Graph subscriptions={subscriptions} tiers={tiers} />
+              ) : (
+                <div className="flex h-full w-full items-center justify-center">
+                  <Loader className="h-10 w-10" />
+                </div>
+              )}
             </div>
           </div>
         </div>
