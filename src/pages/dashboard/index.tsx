55405ad1a029b8e45eb7b9344bfc59e0ba2bdccc
plege-xyz <114194253+plege-xyz@users.noreply.github.com>
2023-01-06 11:47:02 +0530

    wip (#2)

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,10 +7,13 @@ import { App } from "@/components/dashboard/App";
 import { Plege } from "@/utils/plege";
 import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import type { App as AppType } from "plege";
+import { overpass, tt } from "@/utils/fonts";
+import { ShieldExclamationIcon } from "@heroicons/react/20/solid";
 
 const Dashboard = () => {
   const [isCreateAppModalOpen, setIsCreateAppModalOpen] = useState(false);
   const [apps, setApps] = useState<AppType[]>();
+  const [unauthorized, setunauthorized] = useState<boolean>(false);
 
   const refresh = () => {
     setApps(undefined);
@@ -29,7 +32,11 @@ const Dashboard = () => {
 
   useEffect(() => {
     const getApps = async () => {
-      setApps(await Plege(wallet!).app.all());
+      try {
+        setApps(await Plege(wallet!).app.all());
+      } catch {
+        setunauthorized(true);
+      }
     };
 
     if (!apps && wallet) {
@@ -47,19 +54,36 @@ const Dashboard = () => {
           />
         )}
         <div className="flex h-full w-full items-center justify-center">
-          <div className="h-full w-full max-w-screen-xl px-8">
-            <div className="grid grid-cols-3 gap-8 py-10">
-              {!apps ? (
-                <SkeletalLoader />
-              ) : (
-                <>
-                  {apps.map((app, key) => (
-                    <App key={key} name={app.name} publicKey={app.publicKey} />
-                  ))}
-                  <CreateApp openCreateAppModal={openCreateAppModal} />
-                </>
-              )}
-            </div>
+          <div className="flex h-full w-full max-w-screen-xl flex-col px-8">
+            {unauthorized ? (
+              <div className="flex h-[calc(100vh-4rem)] w-full flex-col items-center justify-center">
+                <ShieldExclamationIcon className="h-24 w-24 text-white" />
+                <div className={`${overpass} mt-14 text-center uppercase`}>
+                  plege is currently in private-beta <br />
+                  to get access contact us
+                  <a href="/discord">
+                    <span className="ml-1.5 text-indigo-500">here</span>
+                  </a>
+                </div>
+              </div>
+            ) : (
+              <div className="grid grid-cols-3 gap-8 py-10">
+                {!apps ? (
+                  <SkeletalLoader />
+                ) : (
+                  <>
+                    {apps.map((app, key) => (
+                      <App
+                        key={key}
+                        name={app.name}
+                        publicKey={app.publicKey}
+                      />
+                    ))}
+                    <CreateApp openCreateAppModal={openCreateAppModal} />
+                  </>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </>
