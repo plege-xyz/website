6d0439cbe432959e4a4e41fe455a09425328ef6a
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-01-06 15:45:27 +0530

    wip landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,14 @@ import { Toaster } from "react-hot-toast";
 
 import "../styles/globals.css";
 import { overpass } from "@/utils/fonts";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <Provider>
+      <Head>
+        <title>Plege</title>
+      </Head>
       <Toaster containerClassName={`${overpass}`} />
       <Component {...pageProps} />
     </Provider>
